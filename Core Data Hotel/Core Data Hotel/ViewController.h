253b8f31ed387a4a403668e2fbd61937e54f8d80
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-04-24 11:21:22 -0700

    Start project file

---
+++ Core Data Hotel/Core Data Hotel/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Core Data Hotel
+//
+//  Created by Robert Hatfield on 4/24/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
---
+++ Core Data Hotel/Core Data Hotel/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  Core Data Hotel
+//
+//  Created by Robert Hatfield on 4/24/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+-(void) loadView {
+    [super loadView];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
